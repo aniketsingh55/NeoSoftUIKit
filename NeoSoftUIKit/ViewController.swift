7dd85ab9a30a6aaa6c885bfb33c715732a03b56e
aniketsingh55 <aniketsingh55577@gmail.com>
2025-05-05 22:36:39 +0530

    Initial Commit

---
+++ NeoSoftUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoSoftUIKit
+//
+//  Created by AdminTech02 on 05/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NeoSoftUIKitUITests/NeoSoftUIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NeoSoftUIKitUITestsLaunchTests.swift
+//  NeoSoftUIKitUITests
+//
+//  Created by AdminTech02 on 05/05/25.
+//
+
+import XCTest
+
+final class NeoSoftUIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
